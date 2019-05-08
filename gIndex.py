99bb34ddedbb31169e128889cd9f31e2907da55a
Julio <28212586+HoolioSanchez@users.noreply.github.com>
2019-05-08 08:44:08 -0700

    added rev function

--- gIndex.py
+++ gIndex.py
@@ -12,7 +12,7 @@ import seaborn as sb
 from matplotlib import rcParams
 
 #%%
-rcParams['figure.figsize'] = 10,10
+rcParams['figure.figsize'] = 15,10
 sb.set_style('whitegrid')
 
 graph = Graph()
@@ -42,9 +42,10 @@ gDF = gDF.set_index('Date')
 gDF.head()
 #%%
 aprilData = gDF.loc['2019-04-01': '2019-04-30']
-
+marchData = gDF.loc['2019-03-01': '2019-03-31']
 #%%
 aprilData.head()
+# marchData.head()
 
 #%%
 revCol = 'Ad Revenue ($)'
@@ -52,22 +53,6 @@ appCol = 'App'
 aprilRevData = aprilData[[appCol, revCol]]
 
 graph.plotAppsLineGraph(aprilRevData, 'Ad Rev', 'Date', 'Rev', revCol, appCol)
-
-#%%
-aprilDAU = aprilData[[appCol, revCol, 'DAU']]
-aprilDAU['ARPDAU'] = (aprilDAU[revCol] / aprilDAU['DAU'])
-aprilDAU.head()
-
-#%%
-aprilARPDAU = aprilDAU[[appCol, 'ARPDAU']]
-graph.plotAppsLineGraph(aprilARPDAU, 'ARPDAU', 'Date', 'ARPDAU','ARPDAU', appCol)
-
-#%%
-mdf = pd.DataFrame(googleData)
-mdf = mdf.set_index(['Date', appCol])
-
-testDF = mdf
-testDF = testDF.loc[slice(None), 'DAU']
 
 #%%
 def getARPDAU(dataFrame, appList, revList, breakDownParameter):
@@ -80,12 +65,6 @@ def getARPDAU(dataFrame, appList, revList, breakDownParameter):
     return results
 
 #%%
-def getGrowthRate(presentSet, perviousSet):
-    # (present  - pervious) / pervious X 100 
-    results = ((presentSet - perviousSet) / perviousSet) * 100
-    return results
-
-#%%
 def getAllARPDAU(dataframe, appList, revList, breakdown):
     results = pd.DataFrame()
     appendDF = pd.DataFrame()
@@ -95,6 +74,68 @@ def getAllARPDAU(dataframe, appList, revList, breakdown):
         arpdau = getARPDAU(tempDF, appList, revList, breakdown)
         appendDF = arpdau
         appendDF['App Name'] = app
+
         results = results.append(appendDF)
 
     return results
+#%%
+
+apirlARPDAU = getAllARPDAU(aprilData, appCol, revCol, 'w')
+allDF = getAllARPDAU(gDF, appCol, revCol, 'M')
+
+allDF.head()
+#%%
+def getGrowthARPDAU(dataframe,presentSet, perviousSet):
+    # (present  - pervious) / pervious X 100 
+    #results = ((presentSet - perviousSet) / perviousSet) * 100
+    results = pd.DataFrame()
+
+    for app in apps: 
+            tempDF = dataframe[dataframe['App Name'] == app]
+            tempDF.reset_index()
+            print(tempDF)
+    
+    return results
+
+#%%
+
+def getRateOfChange(dataframe):
+        results = pd.DataFrame()
+        for app in apps: 
+                tempDF = dataframe[dataframe['App Name'] == app]
+                appDF = tempDF
+                appDF['RateOfChange'] = tempDF['ARPDAU'].pct_change()
+                appDF['App Name'] = app
+                results = results.append(appDF)
+
+        return results
+
+def getRevData(dataframe, appList, revList, breakDownParameter):
+        results = pd.DataFrame()
+        totalRev = dataframe[revList].resample(breakDownParameter).sum()
+        results['Total Rev'] = totalRev
+        return results
+
+#%%
+aprilRate = getRateOfChange(apirlARPDAU)
+allRate = getRateOfChange(allDF)
+allRate.head(20)
+# aprilRate.to_csv('rateChange.csv')
+
+#%%
+def getAllRevData(dataframe, appList, revList, breakDownParameter):
+        results = pd.DataFrame()
+        for app in apps: 
+                temp = dataframe[dataframe[appList] == app]
+                totalAdRev = getRevData(temp, appList, revList, breakDownParameter)
+                appendDF = totalAdRev
+                appendDF['App'] = app
+                results = results.append(appendDF)
+        return results 
+
+#%%
+allRevData = gDF[[revCol, appCol]]
+
+totalAppRev = allRevData[revCol].resample('m').sum()
+totalAppRev2 = getAllRevData(allRevData, appCol, revCol, 'm')
+totalAppRev2.to_csv('Month Rev.csv')
